9e1db04999ae83d807c61edbf34376ea1a747600
Aron Griffis <aron@arongriffis.com>
2013-07-30 08:15:25 -0400

    Allow (key, newvalue) pairs in filter_query_params() and filter_headers() so keys can be redacted rather than removed.

--- httreplay/utils.py
+++ httreplay/utils.py
@@ -2,9 +2,6 @@ import urllib
 import urlparse
 
 
-# XXX this is an odd grab-bag. Something more principled?
-
-
 def sort_string(s):
     """A simple little toy to sort a string."""
     return ''.join(sorted(list(s))) if s else s
@@ -19,27 +16,30 @@ def filter_query_params(url, remove_params):
     """
     Remove all provided parameters from the query section of the ``url``.
 
-    :param remove_params: A list of query parameters to scrub from the URL.
+    :param remove_params: A list of (param, newvalue) to scrub from the URL.
     :type remove_params: list
     """
     if not url:
         return url
 
+    remove_params = dict((p, None) if isinstance(p, basestring) else p
+        for p in remove_params)
+
     parsed_url = urlparse.urlparse(url)
-    parsed_qs = urlparse.parse_qs(parsed_url.query)
-    for filter_query_param in remove_params:
-        if filter_query_param in parsed_qs:
-            del parsed_qs[filter_query_param]
-    filtered_qs = urllib.urlencode(parsed_qs)
-    filtered_parse_result = urlparse.ParseResult(
+    parsed_qsl = urlparse.parse_qsl(parsed_url.query, keep_blank_values=True)
+
+    filtered_qsl = [(p, remove_params.get(p, v)) for p, v in parsed_qsl]
+    filtered_qsl = [(p, v) for p, v in filtered_qsl if v is not None]
+
+    filtered_url = urlparse.ParseResult(
         scheme=parsed_url.scheme,
         netloc=parsed_url.netloc,
         path=parsed_url.path,
         params=parsed_url.params,
-        query=filtered_qs,
+        query=urllib.urlencode(filtered_qsl),
         fragment=parsed_url.fragment)
-    filtered_url = urlparse.urlunparse(filtered_parse_result)
-    return filtered_url
+
+    return urlparse.urlunparse(filtered_url)
 
 
 def filter_query_params_key(remove_params):
@@ -56,14 +56,25 @@ def filter_query_params_key(remove_params):
 
 def filter_headers(headers, remove_headers):
     """
-    Remove all desired headers from the provided ``headers`` dict.
+    Remove undesired headers from the provided ``headers`` dict.
+    The header keys are case-insensitive.
 
-    :param remove_headers: A list of header names to remove.
+    :param remove_headers: A list of header names to remove or redact.
     :type remove_headers: list
     """
-    for remove_header in remove_headers:
-        if remove_header in headers:
-            del headers[remove_header]
+    # Upgrade bare 'header' to ('header', None) in remove_headers
+    remove_headers = [(h, None) if isinstance(h, basestring) else h
+        for h in remove_headers]
+
+    # Make remove_headers a dict with lower-cased keys
+    remove_headers = dict((h.lower(), v) for h, v in remove_headers)
+
+    # Replace values in headers with values from remove_headers
+    headers = dict((h, remove_headers.get(h.lower(), v))
+        for h, v in headers.items())
+
+    # Remove any that ended up None
+    headers = dict((h, v) for h, v in headers.items() if v is not None)
     return headers
 
 
